ade873672fcc89577ab9f509aeaa3b9fffb93c7c
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-03-17 16:52:33 +0530

    musix-server-code

---
+++ README.md
@@ -0,0 +1,23 @@
+# start Music server
+
+first configure config folder with firebase serviceAccountKey.json
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`MONGODB_URI` =
+
+## Install Node_modules
+
+```bash
+  npm install
+```
+
+## Run
+
+To Run this project run
+
+```bash
+  npm run dev
+```
---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+require("dotenv/config");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// user authentication routes
+const userRoute = require("./routes/auth");
+app.use("/api/users/", userRoute);
+
+// Artist links
+const artistsRoute = require("./routes/artists");
+app.use("/api/artists/", artistsRoute);
+
+// Album links
+const albumRoute = require("./routes/albums");
+app.use("/api/albums/", albumRoute);
+
+// Songs links
+const songRoute = require("./routes/songs");
+app.use("/api/songs/", songRoute);
+
+// If any depreciation warning add depreciation options
+// mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true }, () => {
+//   console.log("Mongodb Connected");
+// });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "server is running" })
+})
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection
+  .once("open", () => console.log("Connected"))
+  .on("error", (error) => {
+    console.log(`Error : ${error}`);
+  });
+
+app.listen(4000, () => console.log("lisitening to port 4000"));
